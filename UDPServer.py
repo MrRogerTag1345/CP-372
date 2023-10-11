dedce58bd92cd5a76f33e8b6e6b0b2eaf41e5a8e
Chetas Patel <94209839+MrRogerTag1345@users.noreply.github.com>
2023-10-10 21:51:14 -0400

    Update UDPServer.py
    Co-Authored-By: Andy Vuong <90644181+Ancuki@users.noreply.github.com>

--- UDPServer.py
+++ UDPServer.py
@@ -1,8 +1,11 @@
 # Import socket module
-from socket import * 
-import sys # In order to terminate the program
-import random #for random integer 
-import struct 
+from socket import *
+import sys  # In order to terminate the program
+import random  # for random integer
+import struct
+
+# Define the expected packet format
+format = '!IHH'
 
 # Convert an integer to a "short"
 def int_to_short(x):
@@ -10,41 +13,44 @@ def int_to_short(x):
     x = max(min(x, 32767), -32768)
     return x
 
-#  Debugging error message
+# Debugging error message
 def check_server_response(response):
-    data_len, pcode, entity = struct.unpack_from('!IHH', response)
+    data_len, pcode, entity = struct.unpack(format, response)
     if pcode == 555:
         response = response[8:]
-        print(response.decode())
+        print("Received response from the server:", response.decode())
         sys.exit()
-    return
 
 # Assign a port number
-serverPort = 120000
+serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 
 # Bind the socket to server address and server port
 serverSocket.bind(("", serverPort))
 
-
 while True:
+    packet, clientAddress = serverSocket.recvfrom(1024)
 
-	data_length, pcode, entity, data, clientAddress  = serverSocket.recvfrom(1024)
-    # Verfies packet 
-	if (data_length != len(data)) or (data != "Hello World!!!") or (pcode != 0) or (entity != 2):
-		print("Verfication failed") 
-		break
-	
-	# Making values in packet 
-	repeat= random.randint(5, 20)
-	udp_port=random.randint(20000, 30000)
-	len = int_to_short(random.randint(50, 100))
-	codeA = int_to_short(random.randint(100, 400))
+    # Verifies packet
+    data_length, pcode, entity, data = struct.unpack(format, packet)
+    if (data_length != len(packet)) or (data != "Hello World!!!") or (pcode != 0) or (entity != 2):
+        print("Verification failed")
 
-	# Making the packet 
-	packet = struct.pack("iiiiihh",data_length, pcode, entity, repeat, udp_port, len, codeA)
-	
-	serverSocket.sendto(packet.encode(), clientAddress)
+    # Making values in the packet
+    repeat = random.randint(5, 20)
+    udp_port = random.randint(20000, 30000)
+    length = int_to_short(random.randint(50, 100))
+    codeA = int_to_short(random.randint(100, 400))
+    
+    print(data_length)
+    print(pcode)
+    print(entity)
+    print(data)
 
-serverSocket.close()  
-sys.exit() #Terminate the program after sending the corresponding data
+    # Making the packet
+    new_packet = struct.pack(format, data_length, pcode, entity, repeat, udp_port, length, codeA)
+
+    serverSocket.sendto(new_packet, clientAddress)
+
+# Close the server socket (this part may need improvements for graceful termination)
+serverSocket.close()
