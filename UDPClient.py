eab02a9278dc0e9bdeaaaa29691f4eee64ec51bd
Andy Vuong <90644181+Ancuki@users.noreply.github.com>
2023-10-15 23:40:41 -0400

    Update UDPClient.py

--- UDPClient.py
+++ UDPClient.py
@@ -4,6 +4,11 @@ import sys
 import struct 
 import random  # for random integer
 
+def getHeaderData(packet):
+    #header is first 8 bit, rest is data 
+    header=packet[:8]
+    data=packet[8:]
+    return header,data 
 # Printing starting stage A
 print("----------- Starting Stage A -----------")
 #assign server name 
@@ -38,41 +43,42 @@ if(len(packet)) % 4 != 0: #can also do struct.calcsize(format)
 clientSocket.sendto(packet, (serverName, serverPort))
 
 # Getting packet from server 
-new_packet, serverAddress = clientSocket.recvfrom(2024)
-format="! i i i i i h h"
-data_length, pcode, entity, repeat, udp_port, length, codeA =  struct.unpack(format, new_packet)
+serverPacket, serverAddress = clientSocket.recvfrom(2024)
+print("Server send packet")
+header,data=getHeaderData(serverPacket)
+repeat, udp_port, length, codeA =  struct.unpack("!IIHH",data)
+data_length,pcode,entity =  struct.unpack("!IHH",header)
 # Printing contents of packets 
-
 print(f"Received packet from the server: data_len: {data_length} pcode: {pcode} entity: {entity} repeat:{repeat} len: {length} udp_port: {udp_port} codeA: {codeA} ")
 print("----------- End Stage A -----------")
 
-print("----------- Starting Stage B -----------")
-# Initlaizing variables 
-pcode=codeA
-entity = clientPort
-packetId=random.randint(0, repeat-1)
-data=''.zfill(length)
-#ensuring len of data is divisible by 4
-while(len(data)%4!=0):
-    #while not divisble, add a 0 to the end 
-    data.bytearray(1)
-#convert to int from string 
-data=int(data)
-datalength=len(data)+len(packetId)
-format='ihhhi'
-# Making packet 
-packet = struct.pack(format,data_length,pcode,entity,packetId,data)
-# Sending packet to server 
-clientSocket.sendto(packet, (serverName, serverPort))
-recieived=False 
-while recieived==False:
-    socket.settimeout(5)
-    acked_packet, serverAddress = clientSocket.recvfrom(2024)
-    p_code,entity,acked_packet_id=struct.unpack('ihhi', new_packet)
-    #if no packet was received, send packet again 
-    if(acked_packet_id==0):
-        clientSocket.sendto(packet, (serverName, serverPort))
-    else:
-        recieived=True
-        clientSocket.sendto(acked_packet_id, serverAddress)
+# print("----------- Starting Stage B -----------")
+# # Initlaizing variables 
+# pcode=codeA
+# entity = clientPort
+# packetId=random.randint(0, repeat-1)
+# data=''.zfill(length)
+# #ensuring len of data is divisible by 4
+# while(len(data)%4!=0):
+#     #while not divisble, add a 0 to the end 
+#     data.bytearray(1)
+# #convert to int from string 
+# data=int(data)
+# datalength=len(data)+len(packetId)
+# format='ihhhi'
+# # Making packet 
+# packet = struct.pack(format,data_length,pcode,entity,packetId,data)
+# # Sending packet to server 
+# clientSocket.sendto(packet, (serverName, serverPort))
+# recieived=False 
+# while recieived==False:
+#     socket.settimeout(5)
+#     acked_packet, serverAddress = clientSocket.recvfrom(2024)
+#     p_code,entity,acked_packet_id=struct.unpack('ihhi', new_packet)
+#     #if no packet was received, send packet again 
+#     if(acked_packet_id==0):
+#         clientSocket.sendto(packet, (serverName, serverPort))
+#     else:
+#         recieived=True
+#         clientSocket.sendto(acked_packet_id, serverAddress)
 clientSocket.close()
