bb331cae7822e105989bde59aa32f26fb8e8ad71
Andy Vuong <90644181+Ancuki@users.noreply.github.com>
2023-10-17 00:23:46 -0400

    Update UDPServer.py

--- UDPServer.py
+++ UDPServer.py
@@ -10,10 +10,14 @@ serverSocket = socket(AF_INET, SOCK_DGRAM)
 
 # Bind the socket to server address and server port
 serverSocket.bind(("localhost", serverPort))
-phase='A'
 #continue  varaible to know when server job is done 
 cont=True
 
+def getHeaderData(packet):
+    #header is first 8 bit, rest is data 
+    header=packet[:8]
+    data=packet[8:]
+    return header,data 
 
 # Convert an integer to a "short"
 def int_to_short(x):
@@ -54,47 +58,63 @@ def makePacketA(pcode,entity):
     header=struct.pack('!IHH',data_length,pcode,entity)
     # Making the packet
     packet=header+data
-    return packet,udp_port,repeat
+    return packet,udp_port,repeat,codeA
+
+def makeAckPacket(data_length,pcode,entity,repeat):
+    packetId=random.randint(0, repeat-1)
+    header=struct.pack('IHH',data_length,pcode,entity)
+    data=struct.pack("!I",packetId)
+    packet=header+data
+    return packet
+
+def makePacketB(data_length,pcode,entity):
+    tcp_port=random.randint(20000, 30000)
+    codeB=random.randint(100, 400)
+    header=struct.pack('IHH',data_length,pcode,entity)
+    data=struct.pack("!II",tcp_port,codeB)
+    packet=header+data
+    return packet
 
 def main():
     while cont:
+        #Phase A
         valid,data_length,pcode,entity,data,clientAddress=verPacketA()
         if valid:
-            packet,udp_port,reapeat=makePacketA()
+            packet,udp_port,repeat,codeA=makePacketA()
             serverSocket.sendto(packet, clientAddress)
+            #Phase B
+            clientPacket, clientAddress = serverSocket.recvfrom(1024)
+            header,data=getHeaderData(clientPacket)
+            data_length,pcode,entity=struct.unpack("IHH",header)
+            packetId,data=struct.unpack("HI",data)
+            #verfies ;en of packet is correct 
+            valid=True
+            if(len(packet) != struct.calcsize(format) ):
+                print("Error: Packet length is not correct")
+                valid=False
+#           #verifies that the packet_id field is correct
+            elif (packetId<0 or packetId>repeat-1):
+                print("Packet id is incorrect")
+                valid=False
+            #ensure packets arrive in the correct order ???
+            elif (data_length!=len(data)+len(packetId) or pcode !=codeA or entity != 2 or (data!=0 and len(data)%4!=0) ):
+                print("Packet is not in the right order")
+                valid=False
+            #passes all testing, send ack package 
+            if valid:
+                awkPacket=makeAckPacket(data_length,pcode,entity,repeat)
+                recieived=False 
+                while recieived==False:
+                    socket.settimeout(5)
+                    acked_packet_id, clientAddress = serverSocket.recvfrom(1024)
+                    if(acked_packet_id!=packetId):
+                        serverSocket.sendto(awkPacket, clientAddress)
+                    else:
+                        recieived=True
+                #makes actual packet now that awk package was send 
+                packet=makePacketB(data_length,pcode,entity)
+                serverSocket.sendto(awkPacket, clientAddress)
     # Close the server socket (this part may need improvements for graceful termination)
     serverSocket.close()
 if __name__ == "__main__":
     main()
-# match phase:
-#     case 'A':
-
-#     case 'B':
-#         format='ihhhi'
-#         packet, clientAddress = serverSocket.recvfrom(1024)
-#         data_length,pcode,entity,packetId,data=struct.unpack(format, packet)
-#        #ensure what the size the packet should be, using this for now 
-#        #
-#        #!! change later 
-#         if( len(packet) != struct.calcsize(format) ):
-#             print("Error: Packet length is not correct")
-#             break
-#         #verifies that the packet_id field is correct
-#         if(packetId<0 or packetId>repeat-1):
-#             print("Packet id is incorrect")
-#             break
-#         #ensure packets arrive in the correct order ???
-#         if(data_length!=len(data)+len(packetId) or pcode !=codeA or entity != 2 or (data!=0 and len(data)%4!=0) ):
-#             print("Packet is not in the right order")
-#             break
-#         #passes all testing, send ack package 
-#         rec_packet=struct.pack('ihhi',data_length,pcode,entity,packetId)
-#         serverSocket.sendto(rec_packet, clientAddress)
-#         recieived=False 
-#         while recieived==False:
-#             socket.settimeout(5)
-#             acked_packet_id, serverAddress = serverSocket.recvfrom(1024)
-#             if(acked_packet_id!=packetId):
-#                 serverSocket.sendto(rec_packet, clientAddress)
-#             else:
-#                 recieived=True
